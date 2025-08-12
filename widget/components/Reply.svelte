d5522b1523800609aa02bf4cca18065a12683785
brokenalarms <brokenalarms@users.noreply.github.com>
2025-08-13 08:19:21 +0900

    fix button

--- widget/components/Reply.svelte
+++ widget/components/Reply.svelte
@@ -208,7 +208,7 @@
     <button
       type="submit"
       disabled={loading}
-      class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition-transform duration-300 ease-in-out hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed dark:bg-indigo-500 dark:hover:bg-indigo-600"
+      class="my-auto inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition-transform duration-300 ease-in-out hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed dark:bg-indigo-500 dark:hover:bg-indigo-600"
       cusdis-disabled={loading}
     >
       {#if loading}
